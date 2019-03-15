9b0435b24f7cc7940af06066c3abfce8edd1c8cb
HexRebuilt <hexrebuilt@gmail.com>
2019-03-15 10:53:16 +0100

    small changers
    still won't work

--- code/ACA_project.c
+++ code/ACA_project.c
@@ -1,44 +1,37 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 4;
+#define SIZE 4
 
-
-int main{
-
-    // creations of the 2 random matrices 
-    int[][] matrixA= create_Matrix();
-    int[][] matrixB= create_Matrix();
-
-    showMatrix(matrixA);
-    showMatrix(matrixB);
-
-
-
-
-
-
-    return 0;
-}
-
-void showMatrix(int[][] matrix){
+void showMatrix(int (*matrix)[]){
+    printf("\n");
     for(int i=0; i <SIZE;i++ ){
         for(int j=0; j< SIZE; j++){
-            printf("%d\t", matrix[i][j]);
+            printf("%d\t", (*matrix)[j]);
         }
         printf("\n");
     }
 }
 
-int[][] create_Matrix ()
+int create_Matrix ()
 {
     int random[SIZE][SIZE];
     int i, j;
-
     srand(time(NULL));
     for(i = 0; i <SIZE; i++)
         for(j = 0; j< SIZE; j++)
             random[j][j] = rand();
     return random;
+}
+
+
+int main(){
+    // creations of the 2 random matrices 
+    int matrixA[][]= create_Matrix();
+    int matrixB[][]= create_Matrix();
+
+    showMatrix(matrixA);
+    showMatrix(matrixB);
+
+    return 0;
 }
