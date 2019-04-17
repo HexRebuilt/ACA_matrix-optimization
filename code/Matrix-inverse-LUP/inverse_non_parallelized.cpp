54622f9b204f6e93882bde28ded6ac9723deea26
HexRebuilt <32871843+HexRebuilt@users.noreply.github.com>
2019-04-17 09:20:40 +0200

    trying to fix lu inversion

--- code/Matrix-inverse-LUP/inverse_non_parallelized.cpp
+++ code/Matrix-inverse-LUP/inverse_non_parallelized.cpp
@@ -7,8 +7,8 @@
 #include <omp.h>
 
 using namespace std;
-#define SIZE 4
-#define MAXNUMBER 10
+#define SIZE 5
+#define MAXNUMBER 50
 #define MINNUMBER 0
  
 void showMatrix(float **matrix){
@@ -67,22 +67,76 @@ void lu(float **a, float **l, float **u){
 }
 
 void pivoting(float **a, float **p){
-    for (int k = 0; k < SIZE-1; k++)
-    {   int imax = 0;
+    for (int k = 0; k < SIZE-1; k++){   
+        int imax = 0;
         //foreach column i need to find which row has the maximum (in module) value
         for (int j = k; j < SIZE; j++)
         {
             //finding the maximum
             if (abs(a[j][k]) > abs(a[imax][k])){
                 imax = j;
+                //cout <<"\nNew iMax = " << imax;
             }
         }
-        //now i swap the rowa imax and k
-    
+        //now i swap the row imax and k
+        //cout <<"\nSwapping row "<<k << "and "<< imax;
+        swap(a[k],a[imax]);
+        swap(p[k],p[imax]);
     }
 
 
     
+}
+
+void forwardSubst(float **l, float **p, float *y, int column){
+    /**
+     * solving yi=Pi/ sum of Lrow-i
+    */
+    y[0]=p[0][column] /l[0][0];
+
+    for(int i=0; i< SIZE; i++){
+        y[i] = p[i][column];
+        for (int j = 0; j < i; j++)
+        {
+            //the P[i] part has been done before
+            y[i] = y[i] - l[i][j]*y[i-1] ;
+        }
+        y[i] = y[i]/l[i][i];
+    }
+}
+
+void backwardSubst(float **u, float *y, float **a1, int column){
+    /**
+     * solving A^-1i=Pi/ sum of Lrow-i
+    */
+    a1[SIZE-1][column] = y[SIZE-1]/u[SIZE-1][SIZE-1];
+
+    for(int i= SIZE-1; i >= 0; i--){
+        y[i] = a1[i][column];
+        for (int j = SIZE-1; j > i; j--)
+        {
+            y[i] = y[i] - u[i][j]*y[i-1] ;
+        }
+        y[i] = y[i]/u[i][i];
+    }
+}
+
+void findInverse(float **a, float **a1, float **l, float **u, float **p, float *yi){
+    /**
+     * foreach column i solve the system LUai=Pi with the i-th column
+     * by using the forward substitution method
+     * LYi=Pi
+     * Uxi=Yi
+     */
+
+    //i can only parallelize this, so i can do each column indipendentily
+    for(int i=0; i< SIZE; i++){
+        float *y = (float *)malloc(SIZE * sizeof(float*));
+        y = yi;
+        forwardSubst(l,p,y,i);
+        backwardSubst(u,y,a1,i);
+    }
+
 }
 
 int main(void){
@@ -91,9 +145,13 @@ int main(void){
     float **l = (float **)malloc(SIZE * sizeof(float*));
     float **u = (float **)malloc(SIZE * sizeof(float*));
     float **p = (float **)malloc(SIZE * sizeof(float*));
+    float **a1 = (float **)malloc(SIZE * sizeof(float*));
+    float *y = (float *)malloc(SIZE * sizeof(float*)); 
+    
     
     for(int i = 0; i < SIZE; i++){
         a[i] = (float *)malloc(SIZE * sizeof(float));
+        a1[i] = (float *)malloc(SIZE * sizeof(float));
         l[i] = (float *)malloc(SIZE * sizeof(float));
         u[i] = (float *)malloc(SIZE * sizeof(float));
         p[i] = (float *)malloc(SIZE * sizeof(float));
@@ -104,37 +162,49 @@ int main(void){
             for(int j = 0; j < SIZE; j++)
                 p[i][j] = 0;
     #pragma omp parallel
-        for(int i = 0; i < SIZE; i++)
+        for(int i = 0; i < SIZE; i++){
             p[i][i] = 1;
+            y[i] =0;
+        }
     
     
     create_Matrix(a,1);
-    //cout << "\nMatrix A:\n";
-    //showMatrix(b);
+    cout << "\nMatrix A:\n";
+    showMatrix(a);
+    cout << "\nPivoting matrix:\n";
+    showMatrix(p);
     double time= omp_get_wtime();
     
-    /**P*a = l*u and LUx =p
-    where p is the column to pivot of the b matrix and x is a column of the inverse
-    
+    /**
+     * P*a = l*u and LUx =p
+     * where p is the column to pivot of the b matrix and x is a column of the inverse
     */
-    //i pivot a and p before the inversion
-    pivoting(a,b);
+    cout << "\nPivoting....\n";
+    pivoting(a,p);
+
+    /**cout << "\nMatrix A:\n";
+    showMatrix(a);
+    cout << "\nPivoting matrix:\n";
+    showMatrix(p);
+    */
     lu(a,l,u);
      
-    
-    //cout << "\nL matrix:\n";
-    //showMatrix(l);
-    //cout << "\nU matrix:\n";
-    //showMatrix(u);
-
-
-    /**P*a = l*u and LUx =p
-    where p is the column to pivot of the b matrix and x is a column of the inverse
-    
+    /**
+    cout << "\nL matrix:\n";
+    showMatrix(l);
+    cout << "\nU matrix:\n";
+    showMatrix(u);
     */
 
-
-
+   /**
+    * solving the system LUxi=Pi where i is the column and x is the column
+    * of the inverse to find
+    */
+    cout << "\nFinding the inverse...";
+    findInverse(a,a1,l,u,p,y);
+    
+    cout<< "\nMatrix a^-1:\n";
+    showMatrix(a1);
 
     time = omp_get_wtime()-time;
     cout << "\nExecution time: "<< time;
